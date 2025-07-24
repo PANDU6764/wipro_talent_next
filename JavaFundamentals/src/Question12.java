821b9059cb4b93f6a0101defc7bdcfee6eeca2b0
Akurathi chaitanya venkata sai <chaitanyads6764@gmail.com>
2025-07-24 11:02:57 +0530

    Java Fundamentals

---
+++ JavaFundamentals/src/Question12.java
@@ -0,0 +1,14 @@
+import java.util.*;
+@SuppressWarnings("unused")
+public class Question12 {
+
+	public static void main(String[] args) {
+		//Scanner sc= new Scanner(System.in);
+		//int num = sc.nextInt();
+		for(int i=1;i<11;i++) {
+			System.out.print(i+"\t");
+		}
+		
+	}
+
+}
